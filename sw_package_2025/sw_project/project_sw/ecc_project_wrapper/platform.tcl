19372743b8b0cefc8062d80e25511af42d626f86
gustdw <gustdewit@gmail.com>
2025-10-26 22:00:57 +0100

    Ignore auto-generated files and stop tracking them

--- sw_package_2025/sw_project/project_sw/ecc_project_wrapper/platform.tcl
+++
@@ -1,9 +0,0 @@
-platform generate -domains 
-platform generate
-platform generate
-platform clean
-platform generate
-platform generate -domains standalone_domain,zynq_fsbl 
-platform generate -domains standalone_domain,zynq_fsbl 
-platform clean
-platform generate
